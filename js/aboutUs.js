0c5a49b39189dfbe98ae8513cdf9117b2a9348cd
divyagrex <divya@grex.in>
2015-02-20 15:00:47 +0530

    added team page

---
+++ js/aboutUs.js
@@ -0,0 +1,74 @@
+/**
+ * Created by Admin on 2/2/15.
+ */
+
+$(document).ready(function(){
+
+    $(".pictures img").on("click",function(){
+
+        /*alert("hello");*/
+       var active=$(this).hasClass("active");
+        var nextPictures=$(this).parent().parent().next();
+        var contain=nextPictures.hasClass("slide");
+        if(contain==true && active==true){
+            $(".slide").removeClass("slide");
+            $(".active").removeClass("active");
+            $(".show").removeClass("show");
+        }
+
+        else{
+            $(".slide").removeClass("slide");
+            /*nextPictures.addClass("slide");*/
+            $(this).parent().parent().nextAll(".pictures").addClass("slide");
+            $(".show").removeClass("show");
+
+            $(this).next().addClass("show");
+            $(".active").removeClass("active");
+            $(this).addClass("active");
+
+        }
+    });
+
+    $(".curtain").on("click",function(){
+        var active=$(this).prev().prev().hasClass("active");
+        var nextPictures=$(this).parent().parent().next();
+        var contain=nextPictures.hasClass("slide");
+
+
+
+        if(contain==true && active==true){
+            $(".slide").removeClass("slide");
+            $(".curtain").removeClass("curtainShow");
+            $(".active").removeClass("active");
+            $(".show").removeClass("show");
+            $()
+        }
+
+        else{
+            $(".slide").removeClass("slide");
+            nextPictures.addClass("slide");
+            $(this).parent().parent().nextAll().addClass("slide");
+
+            $(".show").removeClass("show");
+
+            $(this).prev().addClass("show");
+            $(".active").removeClass("active");
+
+            $(this).prev().prev().addClass("active");
+
+            $(".curtain").addClass("curtainShow");
+            $(this).removeClass("curtainShow");
+            var moveTo=$(this).parent().parent().prev();
+            $('html, body').animate({
+                scrollTop: moveTo.offset().top
+            }, 1000);
+
+        }
+    });
+
+    $("li").on("click",function(){
+        /*alert("helo");*/
+       $(this).nextAll().css("background","red");
+    });
+
+});
